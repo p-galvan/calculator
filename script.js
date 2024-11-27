26a45047c0e1ea22c1e9b197c5bd5b88969d9f70
p-galvan <p.galvant@gmail.com>
2024-11-26 18:15:25 -0600

    feat: create varibles to hold two nums and operators

--- script.js
+++ script.js
@@ -15,3 +15,14 @@ function multiply(a, b) {
 function divide(a, b) {
     return a / b;
 }
+
+// Declare all three variables to hold parameters of calculator functions
+let numberOne = 0;
+let numberTwo = 0;
+let calcOperator = "";
+
+
+// A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. 
+// Create three variables, one for each part of the operation. You’ll use these variables to update your display later.
+
+// Create a new function operate that takes an operator and two numbers and then calls one of the above functions on the numbers.
