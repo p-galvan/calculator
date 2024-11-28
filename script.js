79e118ae6f19dfce343eaacc5e369766039d9f2b
p-galvan <p.galvant@gmail.com>
2024-11-27 19:00:27 -0600

    feat: edit CSS styles

--- script.js
+++ script.js
@@ -51,4 +51,3 @@ let calcOperator = "";
 // A calculator operation will consist of a number, an operator, and another number. For example, 3 + 5. 
 // Create three variables, one for each part of the operation. You’ll use these variables to update your display later.
 
-// Create a new function operate that takes an operator and two numbers and then calls one of the above functions on the numbers.
